feed0c6f5c61f4ca4c1c6104dd3445d4b931baff
Jan Damm <mail@jandamm.de>
2023-02-21 21:07:28 +0100

    Modernize Swift and dependencies

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "MinifyCSSPublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "MinifyCSSPublishPlugin",
             targets: ["MinifyCSSPublishPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
     ],
     targets: [
         .target(
